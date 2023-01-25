da58c34610eab62423c7412d37088ca579965e5f
Barbara Oleszczuk <basiaoleee@o2.pl>
2023-01-25 21:28:59 +0100

    Add buttons

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+function playGame(playerInput) {
+    clearMessages();
 function getMoveName(argMoveId){
     if(argMoveId == 1){
       return 'kamień';
@@ -62,9 +64,9 @@ else if(randomNumber == 3){
   }*/
 printMessage('Mój ruch to: ' +computerMove);
 
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+/*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
-console.log('Gracz wpisał: ' + playerInput);
+console.log('Gracz wpisał: ' + playerInput);*/
 
 let playerMove = getMoveName(playerInput);
 
@@ -80,3 +82,13 @@ else if(playerInput == '3'){
 printMessage('Twój ruch to: ' + playerMove);
 
 displayResult(computerMove,playerMove);
+}
+document.getElementById('play-rock').addEventListener('click', function(){
+    playGame('1');
+  });
+  document.getElementById('play-paper').addEventListener('click', function(){
+    playGame('2');
+  });
+  document.getElementById('play-scissors').addEventListener('click', function(){
+    playGame('3');
+  });
