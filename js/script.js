7916b411d222ff2b72299c3ff964d27c3f49db0f
Barbara Oleszczuk <basiaoleee@o2.pl>
2023-01-28 17:50:14 +0100

    Add correction

--- js/script.js
+++ js/script.js
@@ -1,17 +1,16 @@
-function playGame(playerInput) {
+  {
+    const playGame= function(playerImput) {
     clearMessages();
-    function getMoveName(argMoveId){
-    if(argMoveId == 1){
-      return 'kamień';
-    } 
-    else if(argMoveId==2){return 'papier';}
-    else if(argMoveId==3){return 'nożyce';}
-    else {
+    const getMoveName= function(argMoveId){
+      if(argMoveId == 1){return 'kamień';} 
+      else if(argMoveId==2){return 'papier';}
+      else if(argMoveId==3){return 'nożyce';}
+      else {
       printMessage('Nie znam ruchu o id ' + argMoveId + '.');
       return 'nieznany ruch';
-    }
+      }
    }
-    function displayResult(argComputerMove, argPlayerMove){
+    const displayResult= function(argComputerMove, argPlayerMove){
     printMessage('Zagrałem ' + argComputerMove + ', a Ty ' + argPlayerMove);
     console.log('moves:', argComputerMove, argPlayerMove);
     if(computerMove == 'kamień' && playerMove == 'papier'){
@@ -47,23 +46,20 @@ function playGame(playerInput) {
    }
 
 
-    let randomNumber = Math.floor(Math.random() * 3 + 1);
+    const randomNumber = Math.floor(Math.random() * 3 + 1);
 
     console.log('Wylosowana liczba to: ' + randomNumber);
 
-    let computerMove = getMoveName(randomNumber);
+    const computerMove = getMoveName(randomNumber);
 
     printMessage('Mój ruch to: ' +computerMove);
 
-    let playerMove = getMoveName(playerInput);
+    const playerMove = getMoveName(playerInput);
+    printMessage('Twój ruch to: ' + playerMove);
 
-
-     printMessage('Twój ruch to: ' + playerMove);
-
-     displayResult(computerMove,playerMove);
+    displayResult(computerMove,playerMove);
 }
-
-   document.getElementById('play-rock').addEventListener('click', function(){
+  document.getElementById('play-rock').addEventListener('click', function(){
     playGame('1');
   });
   document.getElementById('play-paper').addEventListener('click', function(){
@@ -72,3 +68,4 @@ function playGame(playerInput) {
   document.getElementById('play-scissors').addEventListener('click', function(){
     playGame('3');
   });
+}
